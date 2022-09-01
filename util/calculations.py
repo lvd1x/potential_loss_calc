a361205c3572229f9468aa28a044bafa6c11d3e2
levid <alx.levid@gmail.com>
2022-09-01 01:03:15 -0600

    template utils added

---
+++ util/calculations.py
@@ -0,0 +1,19 @@
+### calculations ###
+
+### Returns total salary based on hourly wage
+def monthly_salary_by_hour() -> None:
+    return None
+
+# loss of long term salary based on consistent yearly raise
+def long_term_loss_salaray(initial_salary, percent_raise, years_out):
+    return None
+
+# list of yearly salary losses based on consistent yearly raise
+def long_term_loss_salary_list():
+    return None
+
+# given initial amounts and percent invested, returns gains based on yearly market returns
+def investment_gain(initial_amounts, percent_investments, yearly_market_returns):
+    return None
+
+    
